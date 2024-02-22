35c5a0383f101c6552f0a31fb8f370a640b7ae60
mattheweq <repos@mattheweq.com>
2024-02-22 22:55:51 +0000

    build and deploy v.2.0

---
+++ v2/app.js
@@ -0,0 +1,53 @@
+    let containerBgInput = document.getElementById('containerBg');
+    let wordInInput = document.getElementById('wordIn');
+    let wordOut = document.getElementById('wordOut');
+    let textColorInput = document.getElementById('textColor');
+    let textShadowColorInput = document.getElementById('textShadowColor');
+
+    function sendForm() {
+      let wordInValue = wordInInput.value.trim() || "abcd";
+      let containerBg = containerBgInput.value || "#AE98ED";
+      let textColor = textColorInput.value || "#CDFE43";
+      let textShadowColor = textShadowColorInput.value || "transparent";
+
+      document.body.style.backgroundColor = containerBg;
+      document.querySelector('h1').style.color = textColor;
+      document.querySelector('h1').style.textShadow = `20px 20px 0px ${textShadowColor}`;
+
+      wordOut.textContent = wordInValue;
+
+      adjustFontSize(); 
+    }
+
+
+    function adjustFontSize() {
+      let h1 = document.querySelector('h1');
+      let targetWidth = window.innerWidth * 0.98;
+      let minFontSize = 10;
+      let maxFontSize = 1200;
+      let fontSize;
+
+      // Binary search algorithm
+      while (minFontSize <= maxFontSize) {
+          fontSize = (minFontSize + maxFontSize) / 2;
+          h1.style.fontSize = fontSize + 'px';
+          h1.style.lineHeight = fontSize - 30 + 'px';
+
+          let currentWidth = h1.getBoundingClientRect().width;
+
+          if (currentWidth < targetWidth) {
+              minFontSize = fontSize + 1;
+          } else if (currentWidth > targetWidth) {
+              maxFontSize = fontSize - 1;
+          } else {
+              break;
+          }
+      }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      sendForm(); 
+      adjustFontSize(); 
+    });
+
+    window.onresize = adjustFontSize; 
